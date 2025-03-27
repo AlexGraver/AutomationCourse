be390a8d9d34a887a6b79a36a73a1282813a7979
AleksejsVenidiktovs <git@github.com:AlexGraver/TR.git>
2025-03-27 17:13:03 +0200

    homework_7

---
+++ src/test/java/helpers/WaitHelper.java
@@ -0,0 +1,62 @@
+package helpers;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class WaitHelper {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private FluentWait<WebDriver> fluentWait;
+    private static final int IMPLICIT_WAIT_MILLISEC = 500;
+    private static final int EXPLICIT_WAIT_SEC = 10;
+    private static final int FLUENT_TIMEOUT = 30;
+    private static final int FLUENT_POLLING = 10;
+
+
+    public WaitHelper(WebDriver driver){
+        this.driver = driver;
+        initImplicitWait(IMPLICIT_WAIT_MILLISEC);
+        initWebDriverWait(EXPLICIT_WAIT_SEC);
+    }
+
+    private void initImplicitWait(int millisec){
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(millisec));
+    }
+
+    private void initWebDriverWait(int sec){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(sec));
+    }
+
+    public FluentWait<WebDriver> getFluentWait(){
+        return new FluentWait<>(driver).withTimeout(Duration.ofSeconds(FLUENT_TIMEOUT))
+                .pollingEvery(Duration.ofMillis(FLUENT_POLLING))
+                .ignoring(TimeoutException.class)
+                .ignoring(ElementClickInterceptedException.class);
+    }
+
+
+    public WebElement waitUntilElementIsPresentInDOM(By locator){
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public WebElement waitUntilElementClickable(By locator){
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public WebElement waitUntilElementDisplayed(By locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void waitUntilElementNotDisplayed(By locator){
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+
+
+
+}
---
+++ src/test/java/homework_7/LoadingImages.java
@@ -0,0 +1,64 @@
+package homework_7;
+
+import helpers.WaitHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoadingImages {
+
+    private static final By COMPASS = By.xpath("//img[@id='compass']");
+    private static final By CALENDAR = By.xpath("//img[@id='calendar']");
+    private static final By AWARD = By.xpath("//img[@id='award']");
+    private static final By LANDSCAPE = By.xpath("//img[@id='landscape']");
+
+    private static WaitHelper waitHelper;
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setUp(){
+        driver = new ChromeDriver();
+        waitHelper = new WaitHelper(driver);
+        driver.manage().window().maximize();
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html");
+    }
+
+    @Test
+    void checkCompassDisplayed(){
+        checkImageDisplayed(COMPASS);
+    }
+
+    @Test
+    void checkCalendarDisplayed(){
+        checkImageDisplayed(CALENDAR);
+    }
+
+    @Test
+    void checkCalendarAward(){
+        checkImageDisplayed(AWARD);
+    }
+
+    @Test
+    void checkCalendarLandscape(){
+        checkImageDisplayed(LANDSCAPE);
+    }
+
+    @AfterAll
+    static void tearDown(){
+        driver.quit();
+    }
+
+
+
+    void checkImageDisplayed(By locator){
+        WebElement element = waitHelper.waitUntilElementIsPresentInDOM(locator);
+        Assertions.assertTrue(element.isDisplayed(), "Element " + locator.toString() + " should be displayed");
+    }
+
+}
