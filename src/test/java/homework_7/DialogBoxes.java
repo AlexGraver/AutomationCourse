b638d8c08a1a432dfffdb04d7cc85c8c316a8e42
AleksejsVenidiktovs <git@github.com:AlexGraver/TR.git>
2025-03-27 23:07:27 +0200

    homework_7

--- src/test/java/homework_7/DialogBoxes.java
+++ src/test/java/homework_7/DialogBoxes.java
@@ -17,9 +17,14 @@ public class DialogBoxes {
 
     private static final By ALERT = By.xpath("//button[@id=\"my-alert\"]");
     private static final By CONFIRM = By.xpath("//button[@id=\"my-confirm\"]");
+    private static final By CONFIRM_RESULT = By.xpath("//p[@id=\"confirm-text\"]");
     private static final By PROMPT = By.xpath("//button[@id=\"my-prompt\"]");
+    private static final By PROMPT_RESULT = By.xpath("//p[@id=\"prompt-text\"]");
     private static final By MODAL = By.xpath("//button[@id=\"my-modal\"]");
-
+    private static final By MODAL_CONTENT = By.xpath("//div[@class=\"modal-body\"]");
+    private static final By MODAL_CLOSE = By.xpath("(//button[@data-bs-dismiss=\"modal\"])[1]");
+    private static final By MODAL_SAVE = By.xpath("(//button[@data-bs-dismiss=\"modal\"])[2]");
+    private static final By MODAL_RESULT = By.xpath("//p[@id=\"modal-text\"]");
 
 
     @BeforeAll
@@ -27,6 +32,7 @@ public class DialogBoxes {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         waitHelper = new WaitHelper(driver);
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/dialog-boxes.html");
     }
 
     @AfterAll
@@ -37,14 +43,75 @@ public class DialogBoxes {
     }
 
     @Test
-    void checkAlertTest(){
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/dialog-boxes.html");
+    void switchToAlertTest(){
         driver.findElement(ALERT).click();
         waitHelper.waitUntilAlertIsPresent();
-
         Alert alert = driver.switchTo().alert();
         String text = alert.getText();
+        alert.accept();
         Assertions.assertEquals(text, "Hello world!");
+    }
+
+    @Test
+    void acceptConfirmTest(){
+        driver.findElement(CONFIRM).click();
+        waitHelper.waitUntilAlertIsPresent();
+        Alert alert = driver.switchTo().alert();
+        String text = alert.getText();
+
+        Assertions.assertEquals(text, "Is this correct?");
+        alert.accept();
+        String result = driver.findElement(CONFIRM_RESULT).getText();
+        Assertions.assertEquals(result,"You chose: true");
+    }
+
+    @Test
+    void cancelConfirmTest(){
+        driver.findElement(CONFIRM).click();
+        waitHelper.waitUntilAlertIsPresent();
+        Alert alert = driver.switchTo().alert();
+        String text = alert.getText();
+
+        Assertions.assertEquals("Is this correct?", text);
+        alert.dismiss();
+        String result = driver.findElement(CONFIRM_RESULT).getText();
+        Assertions.assertEquals(result,"You chose: false");
+    }
+
+    @Test
+    void promptTest(){
+        driver.findElement(PROMPT).click();
+        waitHelper.waitUntilAlertIsPresent();
+        Alert alert = driver.switchTo().alert();
+        String text = alert.getText();
+
+        Assertions.assertEquals("Please enter your name", text);
+        alert.sendKeys("Alex");
+        alert.accept();
+        String result = driver.findElement(PROMPT_RESULT).getText();
+        Assertions.assertEquals(result,"You typed: Alex");
+    }
+
+    @Test
+    void modalCloseTest(){
+        driver.findElement(MODAL).click();
+        String text = waitHelper.waitUntilElementDisplayed(MODAL_CONTENT).getText();
+        driver.findElement(MODAL_CLOSE).click();
+        String result = driver.findElement(MODAL_RESULT).getText();
+
+        Assertions.assertTrue(text.contains("This is the modal body"), "Text not as expected");
+        Assertions.assertEquals(result, "You chose: Close");
+    }
+
+    @Test
+    void modalSaveTest(){
+        driver.findElement(MODAL).click();
+        String text = waitHelper.waitUntilElementDisplayed(MODAL_CONTENT).getText();
+        driver.findElement(MODAL_SAVE).click();
+        String result = driver.findElement(MODAL_RESULT).getText();
+
+        Assertions.assertTrue(text.contains("This is the modal body"), "Text not as expected");
+        Assertions.assertEquals(result, "You chose: Save changes");
     }
 
 
