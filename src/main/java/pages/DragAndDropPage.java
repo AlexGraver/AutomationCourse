1402dfb9a2f9a2505939bb73efa7c0b749c6e0e8
AlexGraver <alex.graver88@gmail.com>
2025-04-21 21:06:11 +0300

    homework 10_dragAndDropPage refactor to pageFactory style

--- src/main/java/pages/DragAndDropPage.java
+++ src/main/java/pages/DragAndDropPage.java
@@ -1,32 +1,36 @@
 package pages;
 
 import core.BasePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class DragAndDropPage extends BasePage {
 
-
-    private static final By HEADER_DRAG_DROP = By.xpath("//h1[@class=\"display-6\"]");
-    private static final By DRAG_FROM = By.xpath("//div[@id=\"draggable\"]");
-    private static final By DROP_TO = By.xpath("//div[@id=\"target\"]");
-
-    public DragAndDropPage(WebDriver driver){
+    public DragAndDropPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
-    public WebElement getDraggableElement(){
-        return findElement(DRAG_FROM);
+    @FindBy(xpath = "//h1[@class='display-6']")
+    private WebElement headerDragDrop;
+
+    @FindBy(id = "draggable")
+    private WebElement draggableElement;
+
+    @FindBy(id = "target")
+    private WebElement targetElement;
+
+    public WebElement getDraggableElement() {
+        return draggableElement;
     }
 
-    public WebElement getTargetElement(){
-        return findElement(DROP_TO);
+    public WebElement getTargetElement() {
+        return targetElement;
     }
 
-
-
-
-
-
+    public WebElement getHeader() {
+        return headerDragDrop;
+    }
 }
