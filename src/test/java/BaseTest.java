8caab22ac578d004fa382061f801812a1584ad4a
AleksejsVenidiktovs <alex.graver88@gmail.com>
2025-05-20 22:49:33 +0300

    homework 13.8 try to fix

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import core.driver.TestDriver;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
 import pages.HomePage;
 import testExtensions.ScreenshotOnFailureExtension;
 
@@ -17,9 +18,10 @@ public class BaseTest {
     private static final Configs CONFIGS = ConfigFactory.create(Configs.class);
 
     static public HomePage initUiTest(){
-        new BasePage(TestDriver.getDriver());
+        WebDriver driver = TestDriver.getDriver();
+        new BasePage(driver);
         openPage(CONFIGS.baseUrl());
-        return new HomePage(TestDriver.getDriver());
+        return new HomePage(driver);
     }
 
     static public HomePage initUiTestSelenide(){
