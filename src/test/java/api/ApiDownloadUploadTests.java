6c3b90b31f0670882e0dbf563b3ebe13b921bdf6
AleksejsVenidiktovs <alex.graver88@gmail.com>
2025-06-02 14:27:33 +0300

    homework 15.5 add UploadApiTest

--- src/test/java/api/ApiDownloadUploadTests.java
+++ src/test/java/api/ApiDownloadUploadTests.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ApiDownloadUploadTests {
 
     @Test
-    void testDownloadHttpClient() {
+    void apiDownloadTest() {
         String endpoint = "https://bonigarcia.dev/selenium-webdriver-java/docs/webdrivermanager.png";
         String fileName = "downloaded.png";
 
@@ -24,5 +24,23 @@ public class ApiDownloadUploadTests {
         FileManagerApi.saveFile(response, fileName);
         File downloadedFile = new File(fileName);
         assertTrue(downloadedFile.exists(), "File not exists");
+    }
+
+    @Test
+    void apiUploadTest() {
+        String apiUrl = "https://petstore.swagger.io/v2/pet/1/uploadImage";
+        File file = new File("C:\\Users\\agali\\IdeaProjects\\AutomationCourse\\downloaded.png");
+        Response response =
+                given()
+                        .header("accept", "application/json")
+                        .contentType("multipart/form-data")
+                        .multiPart("file", file, "image/png") // Указываем тип содержимого файла
+                        .when()
+                        .post(apiUrl)
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+        System.out.println("Response: " + response.asString());
     }
 }
